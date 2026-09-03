0e8c0e68f5d8c64e10eb65948d73fa171a908215
Beatriz <bea.marcondes13@gmail.com>
2026-09-03 02:16:38 -0300

    fix: simplifica adaptador vercel no config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import vercel from '@astrojs/vercel';
 export default defineConfig({
   site: 'https://www.lojaentreavemarias.com.br',
   output: 'server',
-  adapter: vercel({
-    functionPerRoute: false,
-  }),
+  adapter: vercel(),
 });
